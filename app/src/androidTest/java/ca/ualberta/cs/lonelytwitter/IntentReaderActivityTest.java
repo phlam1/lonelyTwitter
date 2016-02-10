602b4cc003be07aac0e2b075d16103c8dc60956d
phlam <phlam1@ualberta.ca>
2016-02-09 18:16:40 -0700

    Lab 6 tutorial

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivityTest.java
@@ -1,6 +1,7 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
 import android.widget.TextView;
@@ -16,12 +17,45 @@ public class IntentReaderActivityTest extends ActivityInstrumentationTestCase2{
 
     //
     //
+    public void testSendText(){
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "message 1");
 
+        setActivityIntent(intent);
+        IntentReaderActivity ira = (IntentReaderActivity) getActivity();
+
+        assertEquals("IntentReaderActivity should get the text from intent",
+                "message 1", ira.getText());
+    }
+
+    public void testDisplayText(){
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "message 2");
+
+        setActivityIntent(intent);
+        IntentReaderActivity ira = (IntentReaderActivity) getActivity();
+
+        TextView textView = (TextView) ira.findViewById(R.id.intentText);
+        assertEquals("The text should be displayed", "message 2",
+                textView.getText().toString());
+    }
+
+    public void testDoubleText(){
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "message 3");
+        intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY,
+                IntentReaderActivity.DOUBLE);
+
+        setActivityIntent(intent);
+        IntentReaderActivity ira = (IntentReaderActivity) getActivity();
+        assertEquals("The text should be repeated twice!", "message 3message 3",
+                ira.getText());
+    }
     //
     //
 
     //TODO: Add your code here ...
-//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------mnm
 
 //-------------------------------------------------------------------------------
 }
